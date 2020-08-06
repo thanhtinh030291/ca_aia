2727af16858a68e39f5871d47f59f0fe2cb79632
thanhtinh <thanhtinh030291@gmail.com>
2020-08-06 13:35:15 +0700

    update

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Auth;
+use App\User;
+use App\PaymentHistory;
+use View;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function __construct()
+    {
+        //its just a dummy data object.
+        // Sharing is caring
+        
+        $this->middleware(function ($request, $next) {
+            $user = Auth::user()  ;
+            $listUser = User::pluck('email', 'id');
+            $vision = 1;
+            if($user){
+                $messages = $user->messagesReceiver;
+                $renewToClaim = PaymentHistory::where('notify_renew', 1)->where('created_user',$user->id)->get();
+                View::share('renewToClaim', $renewToClaim);
+                View::share('messages', $messages);
+                View::share('listUser', $listUser);
+                View::share('vision', $vision);
+            }
+            
+            return $next($request);
+        });
+        
+        
+        
+    }
+}
