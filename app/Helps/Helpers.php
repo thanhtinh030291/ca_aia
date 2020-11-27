ace654262df6a00bc30f4b6085cf2dc94e56227b
thanhtinh <thanhtinh030291@gmail.com>
2020-11-27 15:21:28 +0700

    update

--- app/Helps/Helpers.php
+++ app/Helps/Helpers.php
@@ -679,12 +679,13 @@ function getTokenCPS(){
     $now = Carbon\Carbon::now();
     $totalDuration = $startTime->diffInSeconds($now);
     if($setting->token_cps == null || $totalDuration >= 3500){
-        $client = new \GuzzleHttp\Client([
-            'headers' => $headers
-        ]);
-        $response = $client->request("POST", config('constants.api_cps').'get_token' , ['form_params'=>$body]);
-        $response =  json_decode($response->getBody()->getContents());
-        $setting->token_cps = data_get($response , 'access_token');
+        // $client = new \GuzzleHttp\Client([
+        //     'headers' => $headers
+        // ]);
+        // $response = $client->request("POST", config('constants.api_cps').'get_token' , ['form_params'=>$body]);
+        // $response =  json_decode($response->getBody()->getContents());
+        // $setting->token_cps = data_get($response , 'access_token');
+        $setting->token_cps = "a9eb47e79d99b28ee895ddcc22f1b2faed2cca94";
         $setting->save();
     }
     return  $setting->token_cps;
